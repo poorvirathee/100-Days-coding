c0ff406b2ab96f50d24be0d0da3d783d55847386
poorvirathee <Poorvi.32952@stu.upes.ac.in>
2026-09-23 19:57:57 +0530

    Add files via upload

---
+++ day45.1.c
@@ -0,0 +1,21 @@
+//Count frequency of a given character in a string.
+#include <stdio.h>
+int main() {
+    char str[100], ch;
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to count its frequency: ");
+    scanf("%c", &ch);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch) {
+            count++;
+        }
+    }
+
+    printf("The character '%c' appears %d times in the string.\n", ch, count);
+    return 0;
+}   
